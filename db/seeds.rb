3dbc5d7cc16aeb84b65837e049a9e949b26d8c7e
Siddharth Ahuja <ahujasid@gmail.com>
2018-05-23 23:30:27 +0530

    sorta implemented css grids

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-3.times do |categ|
+4.times do |categ|
 
 	Category.create!(
 		name: "Category #{categ}"
@@ -16,17 +16,20 @@ end
 
 
 
-3.times do |comp|
+4.times do |comp|
 
 	Company.create!(
 		name: "Company #{comp}",
-		logo: "http://via.placeholder.com/24x24"
+		logo: "http://via.placeholder.com/24x24",
+		bgcolor: "#000000"
 		)
 
 end
 
 
-Flow.create!(name: "Dropbox Onboarding", category_id: 1, company_id: 1, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/350x200", no_of_images: 3, screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" ) 
-Flow.create!(name: "Airtable Onboarding", category_id: 1, company_id: 2, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/350x200", no_of_images: 3, screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
-Flow.create!(name: "Dropbox Sign Up", category_id: 2, company_id: 1, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/350x200", no_of_images: 3, screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
-Flow.create!(name: "Airtable Sign Up", category_id: 2, company_id: 2, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/350x200", no_of_images: 3, screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
+Flow.create!(name: "Dropbox Onboarding", category_id: 1, company_id: 1, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/241x148", screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" ) 
+Flow.create!(name: "Airtable Onboarding", category_id: 1, company_id: 2, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/241x148", screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
+Flow.create!(name: "Dropbox Sign Up", category_id: 2, company_id: 1, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/241x148", screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
+Flow.create!(name: "Airtable Sign Up", category_id: 2, company_id: 2, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/241x148", screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
+Flow.create!(name: "Google Sign Up", category_id: 2, company_id: 3, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/241x148", screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
+Flow.create!(name: "Google Onboarding", category_id: 1, company_id: 3, tags: "tag1 tag2 tag3", thumbnail_img: "http://via.placeholder.com/241x148", screen: "desktop", images:"http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720 http://via.placeholder.com/1024x720" )
