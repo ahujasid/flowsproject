c05304f6c8c7b04cf6516b6bdba2c360922f4122
Siddharth <siddharth@smallcases-MacBook-Pro-2.local>
2018-11-06 20:55:58 +0530

    reversed force ssl in production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   #Force SSL
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
