4c3eea88f562bee177c36a1a8bfbc42f30c27f2a
Siddharth <siddharth@smallcases-MacBook-Pro-2.local>
2018-11-09 17:06:31 +0530

    disabled turbolinks and added function for anchors in application js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,14 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+
+function testfunc(id_num) {
+   console.log(id_num);
+   var target_id = id_num - 49;
+   document.getElementById(target_id).scrollIntoView(true);
+   return false;
+};
