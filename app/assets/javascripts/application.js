2c4fc5910d3c76d7be52d393cec34707c8ff5f60
Siddharth <siddharth@smallcases-MacBook-Pro-2.local>
2018-11-29 23:54:10 +0530

    shows navbar on show page on scrolling up and added twitter deactivate assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,15 @@ function testfunc(id_num) {
    document.getElementById(target_id).scrollIntoView(true);
    return false;
 };
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("show-navbar").style.top = "0";
+  } else {
+    document.getElementById("show-navbar").style.top = "-96px";
+  }
+  console.log("scroll");
+  prevScrollpos = currentScrollPos;
+}
